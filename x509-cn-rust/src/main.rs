0936e3fb448bc574c8d20e5873154fc637fbe3f7
zoracon <alexis@eff.org>
2022-11-29 09:55:37 -0800

    Use TrustDNS crate

--- x509-cn-rust/src/main.rs
+++ x509-cn-rust/src/main.rs
@@ -1,12 +1,23 @@
 use clap::{Arg, App};
-use dns_lookup::{lookup_host};
+use tracing::info;
+// use dns_lookup::{lookup_host};
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::config::*;
 use std::net::IpAddr;
 use std::fs::File;
 use x509_parser::pem::Pem;
 
 // Resolves host
 fn resolve(host: &str) -> std::io::Result<String> {
-  let ips: Vec<std::net::IpAddr> = lookup_host(host)?;
+  // Construct a new Resolver with default configuration options
+  let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+
+  let response = resolver.lookup_ip(host).unwrap();
+
+  let address = response.iter().next().expect("no addresses returned!");
+
+  info!("Outputting common name...");
+  let ips: IpAddr = address;
   Ok(format!("The common name {:?}, resolves to these IPs: {:?}", host, ips))
 }
 
@@ -14,7 +25,7 @@ fn resolve(host: &str) -> std::io::Result<String> {
 fn main() {
   // Clap boilerplate for command line args
   let matches = App::new("Parse X509")
-    .version("0.1.0")
+    .version("0.2.0")
     .author("Alexis Hancock")
     .about("Validate host from x509 Pem File")
     .arg(Arg::with_name("INPUT")
@@ -27,6 +38,7 @@ fn main() {
   let file = File::open(path).unwrap();
 
   //Pass Pem and Parse Pem
+  info!("Parse pem file...");
   let subject = Pem::read(std::io::BufReader::new(file))
     .unwrap().0
     .parse_x509().unwrap()
@@ -36,6 +48,7 @@ fn main() {
   let host = &subject[3..];
 
   // Validate Host
+  info!("Validating host...");
   match resolve(host) {
     Ok(ok_message) => println!("{:?}", ok_message),
     Err(err) => println!("Error: {:?}", err)
