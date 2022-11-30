512aed37eb2f3331383b20ba1124cd1f68b2c000
zoracon <alexis@eff.org>
2022-11-30 14:21:57 -0800

    Add custom error type

--- articurl/src/main.rs
+++ articurl/src/main.rs
@@ -1,11 +1,29 @@
 use arti_client::{TorClient, TorClientConfig};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use clap::{Arg, App};
+use thiserror::Error;
+use anyhow::{Context, Result};
 
-// TODO: Add Port Option
+// Custom Error Types
+#[derive(Error, Debug)]
+pub enum ArtiError {
+    // Argument was empty
+    #[error("No value given for host")]
+    EmptyHost,
+
+    /// Represents all other cases of `std::io::Error`.
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    ArtiError(#[from] arti_client::Error),
+
+    #[error(transparent)]
+    AnyHowError(#[from] anyhow::Error),
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), ArtiError> {
 
     let matches = App::new("Arti Curl")
     .version("0.1.0")
@@ -18,20 +36,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .get_matches();
 
     let path = matches.value_of("host").unwrap_or("");
-    println!("The host passed is: {}", path);
+
+    if path.is_empty() {
+        return Err(ArtiError::EmptyHost);
+    } else {
+        println!("The host passed is: {}", path);
+    }
 
     let config = TorClientConfig::default();
-    let tor_client = TorClient::create_bootstrapped(config).await?;
+    let tor_client = TorClient::create_bootstrapped(config).await.context(format!("Tor failed to bootstrap"))?;
 
-    let mut stream = tor_client.connect((path, 80)).await?;
+    let mut stream = tor_client.connect((path, 80)).await.context(format!("Unable to connect to domain, possibly incorrect or inactive domain"))?;
     let pathb = format!("GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", path);
     stream
         .write_all(pathb.as_bytes())
-        .await?;
-    stream.flush().await?;
+        .await
+        .context(format!("Stream failed to write"))?;
+    stream.flush().await.context(format!("Stream failed to flush"))?;
 
     let mut buf = vec![];
-    stream.read_to_end(&mut buf).await?;
+    stream.read_to_end(&mut buf).await.context(format!("Stream failed to read to the end"))?;
 
     println!("{}", String::from_utf8_lossy(&buf));
 
