1645ebac525332d05cbd7d247d98849f414bdf89
zoracon <alexis@eff.org>
2022-11-30 14:29:11 -0800

    Add code comments

--- articurl/src/main.rs
+++ articurl/src/main.rs
@@ -1,23 +1,26 @@
 use arti_client::{TorClient, TorClientConfig};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use clap::{Arg, App};
+// Great compliments for error handling
 use thiserror::Error;
 use anyhow::{Context, Result};
 
-// Custom Error Types
+// Custom Error Type
 #[derive(Error, Debug)]
 pub enum ArtiError {
     // Argument was empty
     #[error("No value given for host")]
     EmptyHost,
 
-    /// Represents all other cases of `std::io::Error`.
+    // Represents all other cases of `std::io::Error`.
     #[error(transparent)]
     IOError(#[from] std::io::Error),
 
+    // Represents all other cases of `arti_client::Error`.
     #[error(transparent)]
     ArtiError(#[from] arti_client::Error),
 
+    // Represents all other cases of `anyhow::Error`.
     #[error(transparent)]
     AnyHowError(#[from] anyhow::Error),
 }
@@ -25,6 +28,7 @@ pub enum ArtiError {
 #[tokio::main]
 async fn main() -> Result<(), ArtiError> {
 
+    // Sorry I like Clap.
     let matches = App::new("Arti Curl")
     .version("0.1.0")
     .author("zoonarc <zooanrc@duck.com>")
@@ -35,7 +39,7 @@ async fn main() -> Result<(), ArtiError> {
             .help("host name"))
     .get_matches();
 
-    let path = matches.value_of("host").unwrap_or("");
+    let path = matches.value_of("host").unwrap_or(""); // May not need to touch this since there's an appropriate default value
 
     if path.is_empty() {
         return Err(ArtiError::EmptyHost);
@@ -43,9 +47,11 @@ async fn main() -> Result<(), ArtiError> {
         println!("The host passed is: {}", path);
     }
 
+    // Bootstrap Tor
     let config = TorClientConfig::default();
     let tor_client = TorClient::create_bootstrapped(config).await.context(format!("Tor failed to bootstrap"))?;
 
+    // Tor client connect to host
     let mut stream = tor_client.connect((path, 80)).await.context(format!("Unable to connect to domain, possibly incorrect or inactive domain"))?;
     let pathb = format!("GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", path);
     stream
@@ -54,10 +60,11 @@ async fn main() -> Result<(), ArtiError> {
         .context(format!("Stream failed to write"))?;
     stream.flush().await.context(format!("Stream failed to flush"))?;
 
+    // Format and print back response from host
     let mut buf = vec![];
     stream.read_to_end(&mut buf).await.context(format!("Stream failed to read to the end"))?;
 
     println!("{}", String::from_utf8_lossy(&buf));
 
     Ok(())
-} 
+}
